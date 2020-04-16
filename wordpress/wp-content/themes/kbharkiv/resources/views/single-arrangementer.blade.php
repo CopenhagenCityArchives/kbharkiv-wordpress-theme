903bca242d2921157200b5d6b47d7d57a377a8c3
madshostrup <hello@madshostrup.dk>
2020-04-16 15:15:29 +0200

    single events and employee init

---
+++ wordpress/wp-content/themes/kbharkiv/resources/views/single-arrangementer.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.content-single-'.get_post_type())
+  @endwhile
+
+  @if( have_rows('modules') )
+    @while ( have_rows('modules') )
+      @php the_row() @endphp
+      @include('partials.content-' . str_replace('_', '-', get_row_layout()))
+    @endwhile
+  @endif
+
+  <h6>Lokation</h6>
+  @if( get_field('event_location') )
+    {{ get_field('event_location') }}
+  @endif
+
+  <h6>Start</h6>
+  @if( get_field('event_start') )
+    {{ get_field('event_start') }}
+  @endif
+
+  <h6>Slut</h6>
+  @if( get_field('event_end') )
+    {{ get_field('event_end') }}
+  @endif
+
+  <h6>Køb billet</h6>
+  @if( get_field('event_link') )
+    <a href="{{ get_field('event_link') }}">Køb billet</a>
+  @endif
+
+  <h6>Pris</h6>
+  @if( get_field('event_price') )
+    {{ get_field('event_price') }}
+  @else
+    Gratis
+  @endif
+
+@endsection
---
+++ wordpress/wp-content/themes/kbharkiv/resources/views/single-medarbejdere.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <h6>Titel</h6>
+  @if( get_field('employee_title') )
+    {{ get_field('employee_title') }}
+  @endif
+
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.content-single-'.get_post_type())
+  @endwhile
+
+  @if( have_rows('modules') )
+    @while ( have_rows('modules') )
+      @php the_row() @endphp
+      @include('partials.content-' . str_replace('_', '-', get_row_layout()))
+    @endwhile
+  @endif
+
+  <h6>Email</h6>
+  @if( get_field('employee_email') )
+    {{ get_field('employee_email') }}
+  @endif
+
+  <h6>Tlf.</h6>
+  @if( get_field('employee_phone') )
+    {{ get_field('employee_phone') }}
+  @endif
+
+@endsection
