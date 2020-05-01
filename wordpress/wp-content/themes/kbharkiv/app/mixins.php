01df6694b10aa15c843429d05ef4176d60b3f66f
madshostrup <hello@madshostrup.dk>
2020-05-01 13:42:33 +0200

    Color_theme for archives

--- wordpress/wp-content/themes/kbharkiv/app/mixins.php
+++ wordpress/wp-content/themes/kbharkiv/app/mixins.php
@@ -44,20 +44,36 @@ add_filter( 'wpseo_metabox_prio', function () {
 	return 'low';
 });
 
+function namespace_add_custom_types( $query ) {
+  if( (is_category() || is_tag()) && $query->is_archive() && empty( $query->query_vars['suppress_filters'] ) ) {
+    $query->set( 'post_type', array(
+     'post', 'arrangementer', 'medarbejdere'
+        ));
+    }
+    return $query;
+}
+add_filter( 'pre_get_posts', 'namespace_add_custom_types' );
+
 // Function for returning color theme array. $dark boolean is optional for returning the dark theme color
 function theme_color($dark = 0) {
 	global $post;
-	if (get_field('color_theme')) {
-		return $color_theme = explode(',', get_field('color_theme'))[$dark ? '1' : '0'];
+
+	// if cpt archive or post archive
+	if (is_post_type_archive() || is_home()) {
+		return $color_theme = explode(',', get_field('color_theme', get_post_type() . '_options'))[$dark ? 1 : 0];
+	// if color_theme exists
+	} elseif (get_field('color_theme')) {
+		return $color_theme = explode(',', get_field('color_theme'))[$dark ? 1 : 0];
+	// if top level parent color_theme exists
 	} elseif($post->post_parent) {
 		$ancestors = get_post_ancestors($post->ID);
 		$root = count($ancestors)-1;
 		$parent_id = $ancestors[$root];
-
 		if (get_field('color_theme', $parent_id)) {
-			return $theme_colors = explode(',', get_field('color_theme', $parent_id))[$dark ? '1' : '0'];
+			return $theme_colors = explode(',', get_field('color_theme', $parent_id))[$dark ? 1 : 0];
 		}
+	// else default color theme
 	} else {
-		return ['#ffffff', '#000000'][$dark ? '1' : '0'];
+		return ['#ffffff', '#000000'][$dark ? 1 : 0];
 	}
 }
