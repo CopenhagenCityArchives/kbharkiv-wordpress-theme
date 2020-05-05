863180f91e251ce54613b1217c4ad048172333d9
madshostrup <hello@madshostrup.dk>
2020-05-05 15:10:38 +0200

    hide show child pages

--- wordpress/wp-content/themes/kbharkiv/resources/views/page.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/page.blade.php
@@ -4,27 +4,31 @@
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
 
-    @include('partials.modules')
-
-    @php
-      $args = [
-        'child_of' => $post->ID,
-        'depth' => 2,
-        'title_li' => '',
-        'walker' => new Kbharkiv_Walker_Nav_Children
-    	];
-    	$child_pages = get_pages( $args )
-    @endphp
-
-    @if(!empty($child_pages))
-      <div class="row">
-        {{ wp_list_pages($args) }}
-      </div>
-
-    @elseif (has_post_thumbnail())
+    @if (has_post_thumbnail())
       @php the_post_thumbnail( 'herox2' ); @endphp
     @endif
 
     @include('partials.content-page')
+
+    @include('partials.modules')
+
+    @if( get_field('childpages') == true )
+      @php
+        $args = [
+          'child_of' => $post->ID,
+          'depth' => 2,
+          'title_li' => '',
+          'walker' => new Kbharkiv_Walker_Nav_Children
+        ];
+        $child_pages = get_pages( $args )
+      @endphp
+
+      @if(!empty($child_pages))
+        <div class="row">
+          {{ wp_list_pages($args) }}
+        </div>
+      @endif
+    @endif
+
   @endwhile
 @endsection
