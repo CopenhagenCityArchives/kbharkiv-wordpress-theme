d74ac4140cd5dc3754c8481084bee833d8fc9401
madshostrup <hello@madshostrup.dk>
2020-05-15 13:10:53 +0200

    Newsletter form

--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/modules/content-modules-newsletter.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/partials/modules/content-modules-newsletter.blade.php
@@ -5,9 +5,46 @@
     @php echo get_sub_field('modules_newsletter_copy') @endphp
 
     @if( get_sub_field('modules_newsletter_type') == 'true' )
-      Tilmeld
+      <form action="https://kbenhavns-kommune.clients.ubivox.com/handlers/post/" method="post">
+
+        <input type="hidden" name="action" value="subscribe" />
+        <input type="hidden" name="lists" value="69400" />
+
+        <p>
+          <label for="email_address_id">E-mail-adresse</label>
+          <input type="text" name="email_address" id="email_address_id" />
+        </p>
+
+        <p>
+          <label for="data_E-mail_id">E-mail</label>
+          <input type="text" name="data_E-mail" id="data_E-mail_id" />
+        </p>
+
+        <p>
+          <label for="data_Fulde Navn_id">Navn</label>
+          <input type="text" name="data_Fulde Navn" id="data_Fulde Navn_id" />
+        </p>
+
+        <p>
+          <input type="submit" value="Tilmeld" />
+        </p>
+
+      </form>
     @else
-      Afmeld
+      <form action="https://kbenhavns-kommune.clients.ubivox.com/handlers/post/" method="post">
+        <input type="hidden" name="action" value="unsubscribe" />
+        <input type="hidden" name="lists" value="69400" />
+
+        <p>
+          <label for="email_address_id">E-mail-adresse</label>
+          <input type="text" name="email_address" id="email_address_id" />
+        </p>
+
+        <p>
+          <input type="submit" value="Frameld" />
+        </p>
+
+      </form>
     @endif
   </div>
 </section>
