533fd78fef5266d2ec75fd85c64ed86ae2b078d8
madshostrup <hello@madshostrup.dk>
2020-05-15 15:14:38 +0200

    Remove double email input from newsletter sign up

--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/modules/content-modules-newsletter.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/partials/modules/content-modules-newsletter.blade.php
@@ -10,24 +10,13 @@
         <input type="hidden" name="action" value="subscribe" />
         <input type="hidden" name="lists" value="69400" />
 
-        <p>
-          <label for="email_address_id">E-mail-adresse</label>
-          <input type="text" name="email_address" id="email_address_id" />
-        </p>
+        <label for="email_address_id">E-mail-adresse</label>
+        <input type="text" name="email_address" id="email_address_id" />
 
-        <p>
-          <label for="data_E-mail_id">E-mail</label>
-          <input type="text" name="data_E-mail" id="data_E-mail_id" />
-        </p>
+        <label for="data_Fulde Navn_id">Navn</label>
+        <input type="text" name="data_Fulde Navn" id="data_Fulde Navn_id" />
 
-        <p>
-          <label for="data_Fulde Navn_id">Navn</label>
-          <input type="text" name="data_Fulde Navn" id="data_Fulde Navn_id" />
-        </p>
-
-        <p>
-          <input type="submit" value="Tilmeld" />
-        </p>
+        <input type="submit" value="Tilmeld" />
 
       </form>
     @else
@@ -35,15 +24,10 @@
         <input type="hidden" name="action" value="unsubscribe" />
         <input type="hidden" name="lists" value="69400" />
 
-        <p>
-          <label for="email_address_id">E-mail-adresse</label>
-          <input type="text" name="email_address" id="email_address_id" />
-        </p>
+        <label for="email_address_id">E-mail-adresse</label>
+        <input type="text" name="email_address" id="email_address_id" />
 
-        <p>
-          <input type="submit" value="Frameld" />
-        </p>
-
+        <input type="submit" value="Frameld" />
       </form>
     @endif
   </div>
