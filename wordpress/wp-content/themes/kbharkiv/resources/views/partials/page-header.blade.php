98fa9873741818e8f6649214a4dcc28f82a2eb5f
madshostrup <hello@madshostrup.dk>
2020-05-19 09:39:47 +0200

    Hide page header on frontpage

--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/page-header.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/partials/page-header.blade.php
@@ -1,30 +1,32 @@
-<header class="page-header" style="background-color: {{ theme_color() }}">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-6 col-xl-4">
-        <h1>{!! App::title() !!}</h1>
-      </div>
-      <div class="col-lg-6">
-        {{ the_lead() }}
-      </div>
-      <div class="col-xl-2">
-        @php $authors = get_field('author'); @endphp
+@if (!is_front_page())
+  <header class="page-header" style="background-color: {{ theme_color() }}">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 col-xl-4">
+          <h1>{!! App::title() !!}</h1>
+        </div>
+        <div class="col-lg-6">
+          <p class="lead">{{ get_the_lead() }}<p>
+        </div>
+        <div class="col-xl-2">
+          @php $authors = get_field('author'); @endphp
 
-        @if( $authors )
-          @foreach( $authors as $author )
-            @if ( has_post_thumbnail($author->ID))
-              {!! wp_get_attachment_image(get_post_thumbnail_id($author->ID), ['48', '48'], false, ['class' => 'rounded-circle ']) !!}
-            @endif
+          @if( $authors )
+            @foreach( $authors as $author )
+              @if ( has_post_thumbnail($author->ID))
+                {!! wp_get_attachment_image(get_post_thumbnail_id($author->ID), ['48', '48'], false, ['class' => 'rounded-circle ']) !!}
+              @endif
 
-            <div class="d-flex flex-column">
-                {{ get_field('employee_title', $author->ID) ? get_field('employee_title', $author->ID) : 'Medarbejder' }}
-              <h4>{{ get_the_title( $author->ID ) }}</h4>
-            </div>
-          @endforeach
-        @endif
+              <div class="d-flex flex-column">
+                  {{ get_field('employee_title', $author->ID) ? get_field('employee_title', $author->ID) : 'Medarbejder' }}
+                <h4>{{ get_the_title( $author->ID ) }}</h4>
+              </div>
+            @endforeach
+          @endif
+        </div>
       </div>
     </div>
-  </div>
-</header>
+  </header>
 
-{{ the_breadcrumb() }}
+  {{ the_breadcrumb() }}
+@endif
