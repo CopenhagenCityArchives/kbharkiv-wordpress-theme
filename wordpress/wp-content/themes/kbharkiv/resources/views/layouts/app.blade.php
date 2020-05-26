dec2bc3070f86c724f362585ca47c33fb52b99e9
madshostrup <hello@madshostrup.dk>
2020-05-26 15:39:25 +0200

    Footer styling

--- wordpress/wp-content/themes/kbharkiv/resources/views/layouts/app.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/layouts/app.blade.php
@@ -22,6 +22,25 @@
       <span class="sr-only">Chat</span>
       @include('partials.icon', ['icon' => 'message-square'])
     </button>
+
+    {{-- @if (isset($_POST['submit']))
+      @php
+      $navn = $_POST['navn'];
+      $spm = $_POST['spm'];
+      $spm = preg_replace("/\r|\n/", "", $spm);
+      header("Location:https://kbharkiv.kk.dk/api/start_session?issue_menu=1&codeName=stadsarkiv&c2cjs=1&customer.name='.$navn.'&customer.details='$spm'");
+      @endphp
+    @endif
+
+    <form target="_blank" action="{{the_permalink()}}" method="post" id="chat">
+      <input type="text" name="navn" value="Indtast navn" maxlength="100"><br>
+      <textarea name="spm" maxlength="1024">Indtast spørgsmål
+
+      </textarea>
+      <br>
+      <input id="chatsubmit" type="submit" value="Chat" name="submit"><br>
+    </form> --}}
+
     @php do_action('get_footer') @endphp
     @include('partials.footer')
     @php wp_footer() @endphp
