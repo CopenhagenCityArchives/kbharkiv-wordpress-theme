6ea1e8c224a103559f7939f01cea26b8dfc205c9
madshostrup <hello@madshostrup.dk>
2020-05-27 12:35:14 +0200

    News

--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/content.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/partials/content.blade.php
@@ -1,9 +1,17 @@
-<article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
-  </div>
-</article>
+<a href="{{ get_permalink() }}" class="col-md-3 d-flex align-self-stretch">
+    <article @php post_class() @endphp>
+
+      @if ( has_post_thumbnail())
+        @php the_post_thumbnail( 'herox2', ['class' => 'profile-image'] ) @endphp
+      @endif
+      <header>
+        @include('partials/entry-meta')
+        <h3 class="entry-title">{!! get_the_title() !!}</h3>
+      </header>
+      <div class="entry-summary">
+        {{ get_the_lead(get_the_id()) }}
+      </div>
+
+    </article>
+</a>
+<div class="col-md-1"></div>
