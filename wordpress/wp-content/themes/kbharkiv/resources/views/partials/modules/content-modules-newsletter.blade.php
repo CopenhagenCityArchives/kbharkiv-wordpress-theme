80ef181960484455109f3c877460910b203cc479
madshostrup <hello@madshostrup.dk>
2020-05-27 15:45:05 +0200

    Newsletter bootstrap style

--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/modules/content-modules-newsletter.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/partials/modules/content-modules-newsletter.blade.php
@@ -5,7 +5,7 @@
     @php echo get_sub_field('modules_newsletter_copy') @endphp
 
     @if( get_sub_field('modules_newsletter_type') == 'true' )
-      <form action="https://kbenhavns-kommune.clients.ubivox.com/handlers/post/" method="post">
+      {{-- <form action="https://kbenhavns-kommune.clients.ubivox.com/handlers/post/" method="post">
 
         <input type="hidden" name="action" value="subscribe" />
         <input type="hidden" name="lists" value="69400" />
@@ -18,9 +18,27 @@
 
         <input type="submit" value="Tilmeld" />
 
-      </form>
-    @else
+      </form> --}}
+
       <form action="https://kbenhavns-kommune.clients.ubivox.com/handlers/post/" method="post">
+        <input type="hidden" name="action" value="subscribe" />
+        <input type="hidden" name="lists" value="69400" />
+
+        <div class="form-group">
+          <label for="subscribe_email">E-mail</label>
+          <input type="email" class="form-control" name="email_address" id="subscribe_email" placeholder="Din e-mail">
+        </div>
+        <div class="form-group">
+          <label for="subscribe_name">Navn</label>
+          <input type="text" class="form-control" name="data_Fulde Navn" id="subscribe_name" placeholder="Dit navn">
+        </div>
+        <button type="submit" class="btn btn-primary">Tilmeld</button>
+      </form>
+
+    @else
+
+
+      {{-- <form action="https://kbenhavns-kommune.clients.ubivox.com/handlers/post/" method="post">
         <input type="hidden" name="action" value="unsubscribe" />
         <input type="hidden" name="lists" value="69400" />
 
@@ -28,6 +46,17 @@
         <input type="text" name="email_address" id="email_address_id" />
 
         <input type="submit" value="Frameld" />
+      </form> --}}
+
+      <form action="https://kbenhavns-kommune.clients.ubivox.com/handlers/post/" method="post">
+        <input type="hidden" name="action" value="unsubscribe" />
+        <input type="hidden" name="lists" value="69400" />
+
+        <div class="form-group">
+          <label for="unsubscribe_email">E-mail</label>
+          <input type="email" class="form-control" name="email_address" id="unsubscribe_email" placeholder="Din e-mail">
+        </div>
+        <button type="submit" class="btn btn-primary">Afmeld</button>
       </form>
     @endif
   </div>
