edc9faaecd5a5c27cfcc1e2fc6e5647e7f303805
madshostrup <hello@madshostrup.dk>
2020-05-29 10:52:23 +0200

    Remove double echo pagination

--- wordpress/wp-content/themes/kbharkiv/app/pagination.php
+++ wordpress/wp-content/themes/kbharkiv/app/pagination.php
@@ -57,7 +57,7 @@ function pagination( \WP_Query $wp_query = null, $echo = true, $params = [] ) {
     $pagination = '<nav aria-label="Paginering"><ul class="pagination">';
 
     foreach ( $pages as $page ) {
-      $pagination .= '<li class="page-item' . (strpos($page, 'current') !== false ? ' active' : '') . '"> ' . $page . str_replace('page-numbers', 'page-link', $page) . '</li>';
+      $pagination .= '<li class="page-item' . (strpos($page, 'current') !== false ? ' active' : '') . '"> ' . str_replace('page-numbers', 'page-link', $page) . '</li>';
     }
 
     $pagination .= '</ul></nav>';
