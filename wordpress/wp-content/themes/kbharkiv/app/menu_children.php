9215906ef038fee4ae5e9e029a100099e13d3231
madshostrup <hello@madshostrup.dk>
2020-06-04 14:18:21 +0200

    Remove colored image if no thumbnail for child pages

--- wordpress/wp-content/themes/kbharkiv/app/menu_children.php
+++ wordpress/wp-content/themes/kbharkiv/app/menu_children.php
@@ -7,38 +7,38 @@
  * @uses Walker_Nav_Menu
  */
 
- class Kbharkiv_Walker_Nav_Children extends Walker_Page {
- 	public function start_el( &$output, $item, $depth = 0, $args = array(), $current_page = 0 ) {
- 		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+class Kbharkiv_Walker_Nav_Children extends Walker_Page {
+  public function start_el( &$output, $item, $depth = 0, $args = array(), $current_page = 0 ) {
+    $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
 
-     extract($args, EXTR_SKIP);
+    extract($args, EXTR_SKIP);
 
-     $css_class = array('page-item', $depth == 0 ? 'col-sm-6 col-md-5 col-lg-4 col-xl-3 mb-5' : '');
+    $css_class = array('page-item', $depth == 0 ? 'col-sm-6 col-md-5 col-lg-4 col-xl-3 mb-5' : '');
 
-     $css_class = implode(' ', apply_filters( 'page_css_class', $css_class, $item, $depth, $args, $current_page ));
+    $css_class = implode(' ', apply_filters( 'page_css_class', $css_class, $item, $depth, $args, $current_page ));
 
- 		$tag = $depth == 0 ? 'div' : 'li';
- 		$thumbnail = $depth == 0 ? has_post_thumbnail($item->ID) ? get_the_post_thumbnail($item->ID, 'herox1', ['class' => 'mb-4']) : '<div role="presentation" class="mb-4" style="padding-top: 52.5%; background-color: ' . theme_color(1) . '"></div>' : '';
- 		$headline = $depth == 0 ? 'h3' : 'div';
- 		$icon = $depth == 0 ? '<svg class="icon d-inline-block"><use xlink:href="' . App\asset_path('images/feather-sprite.svg') . '#arrow-right-circle"/></svg>' : '';
+    $tag = $depth == 0 ? 'div' : 'li';
+    $thumbnail = $depth == 0 ? has_post_thumbnail($item->ID) ? get_the_post_thumbnail($item->ID, 'herox1', ['class' => 'mb-4']) : '' : '';
+    $headline = $depth == 0 ? 'h3' : 'div';
+    $icon = $depth == 0 ? '<svg class="icon d-inline-block"><use xlink:href="' . App\asset_path('images/feather-sprite.svg') . '#arrow-right-circle"/></svg>' : '';
 
-     //Modification
-     $content = $link_before . $thumbnail . '<div class="d-flex align-items-center mb-2"><' . $headline .'>' . get_the_title($item->ID) . '</' . $headline .'>' . $icon . '</div>' . $link_after;
- 		$lead = $depth == 0 && null !== get_field('lead', $item->ID) ? '<div>' . get_field('lead', $item->ID) . '</div>' : '';
-     $output .= $indent . '<' . $tag . ' class="' . $css_class . '"><a href="' . get_permalink($item->ID) . '">' . $content . $lead . '</a>';
-     //End Modification
+    //Modification
+    $content = $link_before . $thumbnail . '<div class="d-flex align-items-center mb-2"><' . $headline .'>' . get_the_title($item->ID) . '</' . $headline .'>' . $icon . '</div>' . $link_after;
+    $lead = $depth == 0 && null !== get_field('lead', $item->ID) ? '<div>' . get_field('lead', $item->ID) . '</div>' : '';
+    $output .= $indent . '<' . $tag . ' class="' . $css_class . '"><a href="' . get_permalink($item->ID) . '">' . $content . $lead . '</a>';
+    //End Modification
+  }
+
+  public function end_el( &$output, $item, $depth = 0, $args = null ) {
+    if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+      $t = '';
+      $n = '';
+    } else {
+      $t = "\t";
+      $n = "\n";
+    }
+    $tag = $depth == 0 ? 'div' : 'li';
+    $blank_col = $depth == 0 ? '<div class="d-none d-md-block col-md-1 d-lg-none col-xl-1 d-xl-block"></div>' : '';
+    $output .= "</" . $tag . ">{$n}$blank_col";
  	}
-
- 	public function end_el( &$output, $item, $depth = 0, $args = null ) {
-     if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
-         $t = '';
-         $n = '';
-     } else {
-         $t = "\t";
-         $n = "\n";
-     }
- 		$tag = $depth == 0 ? 'div' : 'li';
- 		$blank_col = $depth == 0 ? '<div class="d-none d-md-block col-md-1 d-lg-none col-xl-1 d-xl-block"></div>' : '';
-     $output .= "</" . $tag . ">{$n}$blank_col";
- 	}
- }	// Kbharkiv_Walker_Nav_Children
+}	// Kbharkiv_Walker_Nav_Children
