6e3118a6035c03eb0efead3af7a8e3368bf138a7
madshostrup <hello@madshostrup.dk>
2020-06-08 08:35:58 +0200

    Assets

--- wordpress/wp-content/themes/kbharkiv/resources/assets/scripts/routes/singleArrangementer.js
+++ wordpress/wp-content/themes/kbharkiv/resources/assets/scripts/routes/singleArrangementer.js
@@ -1,11 +1,10 @@
 export default {
   init() {
     setTimeout(function () {
-      let headlineTop = $('.page-header h1').offset().top;
-      let eventInfoTop = $('.event-info').offset().top;
-      let negativeMargin = eventInfoTop - headlineTop;
-      console.log(eventInfoTop - headlineTop);
+      var headlineTop = $('.page-header h1').offset().top;
+      var eventInfoTop = $('.event-info').offset().top;
+      var negativeMargin = eventInfoTop - headlineTop;
       $('.event-info').css('margin-top', -negativeMargin + 'px');
-    }, 200);
+    }, 150);
   },
 };
