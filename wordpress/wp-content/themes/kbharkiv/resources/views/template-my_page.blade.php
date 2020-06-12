f50190fe3e1388117ec1b49cddaaeb02335af1cf
madshostrup <hello@madshostrup.dk>
2020-06-12 10:30:55 +0200

    Floating cards on my page

--- wordpress/wp-content/themes/kbharkiv/resources/views/template-my_page.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/template-my_page.blade.php
@@ -6,39 +6,37 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
+
     @include('partials.page-header')
 
-    @include('partials.content-single-'.get_post_type())
+    @include('partials.content-single-'.get_post_type(), ['no_header' => true])
 
     @include('partials.modules')
 
     @include('partials.child-pages')
 
     <div class="container-fluid" id="{{ get_field('mypage_app') }}">
-      <div class="row">
-
+      <div class="card-columns">
         @if (have_rows('mypage_card'))
           @while ( have_rows('mypage_card') )
             @php
               the_row();
             @endphp
-            <div class="col-lg-6">
-              <section class="card">
-                <div class="card-header">
-                  {{ get_sub_field('mypage_card_headline') }}
-                </div>
-                @if( get_sub_field('mypage_card_image') )
-                  {!!wp_get_attachment_image( get_sub_field('mypage_card_image'), 'cardx2' )!!}
+            <section class="card">
+              <div class="card-header">
+                {{ get_sub_field('mypage_card_headline') }}
+              </div>
+              @if( get_sub_field('mypage_card_image') )
+                {!!wp_get_attachment_image( get_sub_field('mypage_card_image'), 'cardx2' )!!}
+              @endif
+              <div class="card-body" {!! get_sub_field('mypage_card_angular_directive') ? ' data-sdk-app' : '' !!}>
+                {!! get_sub_field('mypage_card_content') !!}
+                @if( get_sub_field('mypage_card_angular_directive') )
+                  <{!! get_sub_field('mypage_card_angular_directive') !!}>
+                  </{!! get_sub_field('mypage_card_angular_directive') !!}>
                 @endif
-                <div class="card-body" {!! get_sub_field('mypage_card_angular_directive') ? ' data-sdk-app' : '' !!}>
-                  {!! get_sub_field('mypage_card_content') !!}
-                  @if( get_sub_field('mypage_card_angular_directive') )
-                    <{!! get_sub_field('mypage_card_angular_directive') !!}>
-                    </{!! get_sub_field('mypage_card_angular_directive') !!}>
-                  @endif
-                </div>
-              </section>
-            </div>
+              </div>
+            </section>
           @endwhile
         @endif
       </div>
