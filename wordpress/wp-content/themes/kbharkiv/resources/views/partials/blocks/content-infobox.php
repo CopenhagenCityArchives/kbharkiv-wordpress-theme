80045032031bbeed08d32904b0999a0bd7970ab0
madshostrup <hello@madshostrup.dk>
2020-07-03 10:44:48 +0200

    Append to child pages

--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/blocks/content-infobox.php
+++
@@ -1,4 +0,0 @@
-<?php
-if(function_exists('get_field')) { ?>
-  <aside class="infobox darken-on-scroll"><?php echo get_field('block_infobox') ?></aside>
-<?php } ?>
--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/child-pages.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/partials/child-pages.blade.php
@@ -15,6 +15,32 @@
         <span class="sr-only">Undersider</span>
         <div class="row">
           {{ wp_list_pages($args) }}
+
+          @if( have_rows('childpages_append') )
+            @while( have_rows('childpages_append') )
+              @php
+                the_row();
+                $image = get_sub_field('childpages_append_image');
+                $link = get_sub_field('childpages_append_link');
+                $text = get_sub_field('childpages_append_text');
+              @endphp
+
+              <div class="page-item col-sm-6 col-md-5 col-lg-4 col-xl-3 mb-5">
+                <a class="article-link" href="{{ $link['url'] }}" target="{{ $link['target'] ? $link['target'] : '_self' }}">
+                  @if($image)
+                    {!! wp_get_attachment_image($image, 'herox1', false, ['class' => 'mb-4']) !!}
+                  @endif
+                  <h3 class="mb-2">
+                    <span class="mr-2">{{$link['title']}}</span>
+                    @include('partials.icon', ['icon' => 'arrow-right-circle'])
+                  </h3>
+                  <div>{{ $text ? $text : '' }}</div>
+                </a>
+              </div>
+              <div class="d-none d-md-block col-md-1 d-lg-none col-xl-1 d-xl-block"></div>
+            @endwhile
+
+          @endif
         </div>
       </div>
     </section>
