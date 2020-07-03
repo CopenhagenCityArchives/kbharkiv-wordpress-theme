0c8b7df48723dea5578447691ca4b09283373fb2
madshostrup <hello@madshostrup.dk>
2020-07-03 09:31:31 +0200

    Remove console.log

--- wordpress/wp-content/themes/kbharkiv/resources/assets/scripts/routes/templateSearchsdk.js
+++ wordpress/wp-content/themes/kbharkiv/resources/assets/scripts/routes/templateSearchsdk.js
@@ -2,13 +2,9 @@ export default {
   init() {
     const sdk = $('.sdk-search')[0];
     const config = { attributes: false, childList: true, subtree: true };
-    console.log('init');
 
     const callback = function() {
-      console.log('change');
-
       if ($('.page-header').length) {
-        console.log('page-header found');
         let bgDefault = $('.top-menu').data('color');
         $('.page-header').css('background-color', bgDefault);
         observer.disconnect();
