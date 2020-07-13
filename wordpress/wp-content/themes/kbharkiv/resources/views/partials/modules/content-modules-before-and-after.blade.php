ae44041089a11b34f53dc871b1dbdab0691393fd
madshostrup <hello@madshostrup.dk>
2020-07-13 11:15:01 +0200

    Remove slider id

--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/modules/content-modules-before-and-after.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/partials/modules/content-modules-before-and-after.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
       <div class="row">
         <figure class="col-lg-8 offset-lg-2">
-          <div id="slider" class="beer-slider" data-beer-label="{{ get_sub_field('modules_before_and_after_after') ? get_sub_field('modules_before_and_after_after') : 'Efter' }}">
+          <div class="beer-slider" data-beer-label="{{ get_sub_field('modules_before_and_after_after') ? get_sub_field('modules_before_and_after_after') : 'Efter' }}">
             {!! wp_get_attachment_image( $images[0]['ID'], 'herox2' ) !!}
             <div class="beer-reveal" data-beer-label="{{ get_sub_field('modules_before_and_after_before') ? get_sub_field('modules_before_and_after_before') : 'Før' }}">
               {!! wp_get_attachment_image( $images[1]['ID'], 'herox2' ) !!}
