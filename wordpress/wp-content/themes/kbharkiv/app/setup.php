6f8931204a17e0a8296f36334667bdefb5fdc566
Bo Henriksen <stumpdenmark@hotmail.com>
2020-09-07 22:15:35 +0200

    Simplify front page searches (starbas and persons)
    Also support GA

--- wordpress/wp-content/themes/kbharkiv/app/setup.php
+++ wordpress/wp-content/themes/kbharkiv/app/setup.php
@@ -14,14 +14,6 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 
-    // SDK kbharkiv
-    //wp_enqueue_style('sdk.css', 'https://www.kbhkilder.dk/software/kildetaster-new-site/resources/css/sdk.css');
-    //wp_enqueue_script('sdk.js', 'https://www.kbhkilder.dk/software/kildetaster-new-site/resources/js/sdk.js', [], null);
-
-    // Starbas
-    // Added to common.js
-    // wp_enqueue_script('starbas.js', 'http://www.kbharkiv.dk/kbharkiv/js/frontpage_searches.js');
-
     if (is_single() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
--- wordpress/wp-content/themes/kbharkiv/resources/assets/scripts/routes/components/searchPerson.js
+++ wordpress/wp-content/themes/kbharkiv/resources/assets/scripts/routes/components/searchPerson.js
@@ -5,6 +5,16 @@ export function searchPerson() {
     var term = document.getElementById('person_search_term').value;
 
     if (term && term != '') {
+      
+      if(window.ga){
+        window.ga('send', {
+          hitType: 'event',
+          eventCategory: 'frontpage_search',
+          eventAction: 'persons',
+          eventLabel: term,
+        });
+      }
+
       var newUrl = window.location.origin + '/brug-samlingerne/soeg-i-indtastede-kilder' + '/results?q1.f=freetext_store&q1.op=in_multivalued&q1.t=' + term + '&sortField=lastname&sortDirection=asc&postsPrPage=10&collections=1,17,18,19';
       window.open(newUrl);
     } else {
