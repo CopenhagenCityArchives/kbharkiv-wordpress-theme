3657b76a9495a88dcce070e03716468e7f9f78aa
Bo Henriksen <stumpdenmark@hotmail.com>
2020-09-10 13:45:52 +0200

    GA site search event
    Add onSubmit event for site search form

---
+++ wordpress/wp-content/themes/kbharkiv/resources/assets/scripts/routes/components/searchSite.js
@@ -0,0 +1,19 @@
+export function searchSite() {
+    $('#searchform_site').submit(function(event){
+      
+      var query = $('#searchform_site-query').val().trim();
+      
+      if(query.length > 0){
+        if(window.ga){
+          window.ga('send', {
+            hitType: 'event',
+            eventCategory: 'frontpage_search',
+            eventAction: 'site',
+            eventLabel: query,
+          });
+        }
+  
+      }
+    });
+  }
+  
