6a2c55cb3eebfb3f0d7008b22b5a456b91787767
madshostrup <hello@madshostrup.dk>
2021-02-05 15:33:35 +0100

    Remove auto hyphens on footer labels
    https://trello.com/c/DIFknzht/345-footer-telefon-er-blevet-todelt

--- wordpress/wp-content/themes/kbharkiv/resources/views/partials/footer.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/partials/footer.blade.php
@@ -13,11 +13,11 @@
 
         <dl class="row small">
           @if( get_field('footer_email', 'option') )
-            <dt class="col-3 col-lg-2">Email</dt>
+            <dt class="col-3 col-lg-2 no-hyphens">Email</dt>
             <dd class="col-9"><a class="text-break" aria-label="Email {!! get_bloginfo() !!}" href="mailto:{{get_field('footer_email', 'option')}}" target="_blank">{{get_field('footer_email', 'option')}}</a></dd>
           @endif
           @if( get_field('footer_phone', 'option') )
-            <dt class="col-3 col-lg-2">Telefon</dt>
+            <dt class="col-3 col-lg-2 no-hyphens">Telefon</dt>
             <dd class="col-9"><a class="text-break" aria-label="Ring til {!! get_bloginfo() !!}" href="tel:{{get_field('footer_phone', 'option')}}" target="_blank">{{get_field('footer_phone', 'option')}}</a></dd>
           @endif
         </dl>
