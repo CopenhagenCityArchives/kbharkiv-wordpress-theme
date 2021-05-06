696a3ea75b5bc6b140a11077fd7eb6d7e3f85fc3
Bo Henriksen <stumpdenmark@hotmail.com>
2021-05-06 14:41:47 +0200

    Revert "Test fix on search error"
    This reverts commit 6070a2448684935517e2786025064e7496cbd6e2.

--- wordpress/wp-content/themes/kbharkiv/resources/views/layouts/app.blade.php
+++ wordpress/wp-content/themes/kbharkiv/resources/views/layouts/app.blade.php
@@ -12,7 +12,22 @@
     <div class="wrap{{is_page_template('views/template-searchsdk.blade.php') ? '' : '' }}" role="document">
       <div id="content">
         <main class="main">
+          @if(!post_password_required())
             @yield('content')
+          @else
+            @include('partials.page-header')
+            <section class="content">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-lg-8 col-xl-6 offset-lg-1">
+                    <div class="entry-content">
+                      {!! get_the_password_form() !!}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          @endif
         </main>
       </div>
     </div>
